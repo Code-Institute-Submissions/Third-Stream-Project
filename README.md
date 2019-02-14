1589f4f611ad7f25f899847f3c4ada40235af486
Donal Mongey <donalmongey@gmail.com>
2019-02-14 17:20:11 +0000

    File name changes, readme updated, slight styling changes

--- README.md
+++ README.md
@@ -1,18 +1,108 @@
+# Maacro Tracker
 
-     ,-----.,--.                  ,--. ,---.   ,--.,------.  ,------.
-    '  .--./|  | ,---. ,--.,--. ,-|  || o   \  |  ||  .-.  \ |  .---'
-    |  |    |  || .-. ||  ||  |' .-. |`..'  |  |  ||  |  \  :|  `--, 
-    '  '--'\|  |' '-' ''  ''  '\ `-' | .'  /   |  ||  '--'  /|  `---.
-     `-----'`--' `---'  `----'  `---'  `--'    `--'`-------' `------'
-    ----------------------------------------------------------------- 
+Data Centric Development Project - Code Institute
+
+Macro Tracker is an app where users can easily check the nutrional information of certain foods, and can can put together meals that will show them the total calories and macro-nutrionents of the dish. 
+The app allows users to:
+
+* Add meats with an image
+* Add/ edit meat cuts and their nutrional values
+* Add/edit vegetables and their nutrional values
+* Create a meal
+
+It was built in Python3 using the Flask microframework.
+
+A live version can be found [Here](https://dmongey-meat-nutrition-recipes.herokuapp.com/).
+
+## UX
+
+The colour theme is primarily indian red and white. The indian red matches with the fruit in the background to make it easy on the eye. The colour red is used for some buttons to make them more prominent on the screen. The overall layout is quite simple and users can access all parts of the site by using any of the links on the navigation bar.
+
+The homepage gives a brief description of the app, which will allow the user to then use the app with ease.
+
+### User Stories:
+
+When a user enters the site, the will immediately have an understanding of the purpose of the app after reading the brief description. They can then easily navigate around the site using the appropriate links in the navbar and buttons on each page. They can proceed to easily view, add or edit any food that that they wish. 
+
+If I want to add a new food, the forms are very easy to find and use with a minimalist design, another feature that makes for a positive user experience.
+
+### Wireframing
+    
+Wireframes were made on pen and paper and can be found in the wireframe folder in the main project file.
+
+## Features
+
+### Existing Features
+
+* Easy navigation around the site via the navbar.
+* Cards representing each food and meal type.
+* A collapsible list to display meat nutritional information.
+* A card reveal feature to display vegetable nutrional information.
+* Various forms allow users to add/edit meats, cuts, vegetables and meals.
+
+### Features left to implement
+
+* Currently when adding a food to a meal, all values are per 100g. I would like to change this so the user can specify how much of each food   will be in a recipe.
+
+## Technologies Used
+
+* HTML
+* CSS
+* Bootstrap
+* Materialize - front-end framework used due to its user experience focus and ease of use.
+* JavaScript
+* JQuery - used to simplify DOM manipulation.
+* Python 3
+* Flask - Flask is a microframework for Python and was used due to its flexibility and simplicity, making it a suitable framework for this     app.
+* MongoDB - MongoDB is a document database
+* mLab – Database-as-a-Service for MongoDB, used to store goals and their data
+
+## Testing
+
+The site was tested on 15" and 13" laptop screens and on the following phone screens sizes: iPhone 5s/6/7/8/X; Galaxy S5, Pixel 2; and Pixel 2XL. It was also tested on iPad and iPad Pro screen sizes. The site is responsive and working on all screen sizes.
+
+All testing was performed manually to ensure:
+
+* Links work correctly.
+* Form submissions work as intended.
+* Model relationships work correctly.
+* The site is responsive across all screen sizes and devices.
+
+The following forms are posted correctly and the database is updated accordingly:
+
+* Add Meat
+* Add Cut
+* Add Vegetable
+* Add Meal
+* Edit Cut
+* Edit Vegetable
+
+The user is redirected to the correct pages after forms are posted. All new food and meal items are displayed placed on the relevent pages.
+
+## Deployment
+
+The site is hosted on heroku.
+
+In order to do this I created a new app on the heroku site and linked to the app via the cloud9 terminal.
+I created a Procfile and requirements.txt file which I then pushed to heroku with the main files.
+On Heroku I have set the config vars to 0.0.0.0 for the IP address and 5000 for the PORT to enable the site to work.
+Heroku will also need config vars for the database name (MONGO_DBNAME) and database uri (MONGO_URI) in order to access and save data.
+
+### Run Locally
+
+To run this site locally, in your terminal enter: git clone https://dmongey101.github.io/Who-am-I/
+
+## Credits
+
+All javascript features were taken from materialize. https://materializecss.com/
+
+### Content
+
+Inspiration for the homapage content came from https://www.myfitnesspal.com/
+
+### Media
+
+* Icons used were obtained from Materialize.
+* Background image retrieved from google images.
 
 
-Hi there! Welcome to Cloud9 IDE!
-
-To get you started, create some files, play with the terminal,
-or visit http://docs.c9.io for our documentation.
-If you want, you can also go watch some training videos at
-http://www.youtube.com/user/c9ide.
-
-Happy coding!
-The Cloud9 IDE team
--- meats_and_recipes.py
+++ meats_and_recipes.py
@@ -12,27 +12,28 @@ app.config["MONGO_URI"] = "mongodb://dmongey:Password1@ds247223.mlab.com:47223/d
 
 mongo = PyMongo(app)
 
+# ----------- Homepage -----------------
 
-def get_category_names():
-    categories = []
-    for category in mongo.db.collection_names():
-        if not category.startswith("system.") and not category.startswith("animals") and not category.startswith("vegetables") and not category.startswith("images") and not category.startswith("recipes"):
-            categories.append(category)
-    return categories  
 
 @app.route("/")
 def show_macros():
     return render_template("homepage.html")
-    
+
+# ------------- List the meats -----------
+
+
 @app.route("/meats")
 def show_meats():
-    animals = mongo.db["images"].find({"animal_name": { "$exists": True }})
+    animals = mongo.db["images"].find({"animal_name": {"$exists": True}})
     return render_template("meats.html", animals=animals)
-    
+
+# ------------ Add a meat form -------------
+
+
 @app.route("/add_animal", methods=["POST", "GET"])
 def add_animal():
     if request.method == "POST":
-        image = request.files['image']  
+        image = request.files['image']
         image_string = base64.b64encode(image.read()).decode("utf-8")
         form_values = request.form.to_dict()
         form_values["image"] = "data:image/png;base64," + image_string
@@ -40,46 +41,63 @@ def add_animal():
         return redirect("/meats")
     else:
         return render_template("add_meat.html")
-        
-@app.route("/<animal_name>/cuts") 
+
+# -------------- List the Cuts -----------------
+
+
+@app.route("/<animal_name>/cuts")
 def show_cuts(animal_name):
-    animal = mongo.db["animals"].find({"animal_name" : animal_name})
+    animal = mongo.db["animals"].find({"animal_name": animal_name})
     return render_template("cuts.html", animal=animal, animal_name=animal_name)
-    
+
+# ------------------- Add a cut form --------------
+
+
 @app.route("/<animal_name>/add_cut", methods=["POST", "GET"])
 def add_cut(animal_name):
     if request.method == "POST":
         form_values = request.form.to_dict()
         mongo.db["animals"].insert_one(form_values)
         return redirect(url_for("show_cuts", animal_name=animal_name))
-    else: 
+    else:
         return render_template("add_cut.html", animal_name=animal_name)
-        
-@app.route("/<animal_name>/<cut_id>/edit/cut", methods=["POST", "GET"])        
+
+# ----------------- Edit a cut -------------------------
+
+
+@app.route("/<animal_name>/<cut_id>/edit/cut", methods=["POST", "GET"])
 def edit_cut(animal_name, cut_id):
     if request.method == "POST":
         form_values = request.form.to_dict()
         mongo.db["animals"].update({"_id": ObjectId(cut_id)}, form_values)
-        
+
         if form_values["animal_name"] != animal_name:
             the_cut = mongo.db["animals"].find_one({"_id": ObjectId(cut_id)})
             mongo.db["animals"].remove(the_cut)
             mongo.db[form_values["animal_name"]].insert(the_cut)
-            
-        return redirect(url_for("show_cuts", animal_name=form_values["animal_name"]))
+            animal_name = form_values["animal_name"]
+
+        return redirect(url_for("show_cuts", animal_name=animal_name))
     else:
-        the_cut =  mongo.db["animals"].find_one({"_id": ObjectId(cut_id)})
+        the_cut = mongo.db["animals"].find_one({"_id": ObjectId(cut_id)})
         return render_template('edit_cut.html', cut=the_cut)
-    
+
+# ----------------- List vegetables ----------------------
+
+
 @app.route("/vegetables")
 def show_vegetables():
-    vegetables = mongo.db["vegetables"].find({"vegetable_name": { "$exists": True }})
+    vegetables = mongo.db["vegetables"].find({"vegetable_name":
+                                             {"$exists": True}})
     return render_template("vegetables.html", vegetables=vegetables)
-    
+
+# ------------------- Add vegetable form --------------------
+
+
 @app.route("/add_vegtable", methods=["POST", "GET"])
 def add_vegetable():
     if request.method == "POST":
-        image = request.files['image']  
+        image = request.files['image']
         image_string = base64.b64encode(image.read()).decode("utf-8")
         form_values = request.form.to_dict()
         form_values["image"] = "data:image/png;base64," + image_string
@@ -88,58 +106,83 @@ def add_vegetable():
     else:
         return render_template("add_vegetable.html")
 
-@app.route("/<vegetable_name>/<vegetable_id>/edit/", methods=["POST", "GET"])        
+# ----------------- Edit vegetable form ---------------------
+
+
+@app.route("/<vegetable_name>/<vegetable_id>/edit/", methods=["POST", "GET"])
 def edit_vegetable(vegetable_name, vegetable_id):
     if request.method == "POST":
         form_values = request.form.to_dict()
-        mongo.db["vegetables"].update({"_id": ObjectId(vegetable_id)}, form_values)
+        mongo.db["vegetables"].update({"_id":
+                                      ObjectId(vegetable_id)}, form_values)
         return redirect(url_for("show_vegetables"))
     else:
-        the_vegetable =  mongo.db["vegetables"].find_one({"_id": ObjectId(vegetable_id)})
-        return render_template('edit_vegetable.html', vegetable=the_vegetable)
- 
-    
-@app.route("/recipes")
-def show_recipes():
-    recipes = mongo.db["images"].find({"recipe_name": { "$exists": True }})
-    return render_template("recipes.html", recipes=recipes)
-        
-        
+        vegetable = mongo.db["vegetables"].find_one({"_id":
+                                                     ObjectId(vegetable_id)})
+        return render_template('edit_vegetable.html', vegetable=vegetable)
+
+# --------------------- List meals --------------------------
+
+
+@app.route("/meal")
+def show_meals():
+    meals = mongo.db["images"].find({"meal_name": {"$exists": True}})
+    return render_template("meals.html", meals=meals)
+
+# --- Saves nutrition info of meat for a meal in dictionary form ---
+
+
 def meat_nutrition_info(meat):
     animal, cut = meat.split(" ")
-    ingredient = mongo.db["animals"].find_one({"animal_name" : animal, "cut_name" : cut})
-    return { "carbs" : ingredient["carbohydrates"], "fat" : ingredient["fat"], "protein" : ingredient["protein"], "calories" : ingredient["calories"] }
-    
+    ingredient = mongo.db["animals"].find_one({"animal_name": animal,
+                                               "cut_name": cut})
+    return {"carbs": ingredient["carbohydrates"],
+            "fat": ingredient["fat"],
+            "protein": ingredient["protein"],
+            "calories": ingredient["calories"]}
+
+# --- Saves nutrition info of veg for a meal in dictionary form ---
+
+
 def veg_nutrition_info(veg):
-    ingredient = mongo.db["vegetables"].find_one({"vegetable_name" : veg})
-    return { "carbs" : ingredient["carbohydrates"], "fat" : ingredient["fat"], "protein" : ingredient["protein"], "calories" : ingredient["calories"] }
-        
-@app.route("/add_recipe", methods=["POST", "GET"])
-def add_recipe():
+    ingredient = mongo.db["vegetables"].find_one({"vegetable_name": veg})
+    return {"carbs": ingredient["carbohydrates"],
+            "fat": ingredient["fat"],
+            "protein": ingredient["protein"],
+            "calories": ingredient["calories"]}
+
+# -------------- Add meal form --------------------
+
+
+@app.route("/add_meal", methods=["POST", "GET"])
+def add_meal():
     if request.method == "POST":
-        image = request.files['image']  
+        image = request.files['image']
         image_string = base64.b64encode(image.read()).decode("utf-8")
         image_collection = request.form.to_dict()
         image_collection["image"] = "data:image/png;base64," + image_string
-        recipe_collection = request.form.to_dict()
+        meal_collection = request.form.to_dict()
         meats = request.form.getlist("meats")
-        recipe_collection["meats"] = {meat : meat_nutrition_info(meat) for meat in meats}
+        meal_collection["meats"] = {meat: meat_nutrition_info(meat)
+                                    for meat in meats}
         vegetables = request.form.getlist("vegetables")
-        recipe_collection["vegetables"] = {veg : veg_nutrition_info(veg) for veg in vegetables}
+        meal_collection["vegetables"] = {veg: veg_nutrition_info(veg)
+                                         for veg in vegetables}
         herbs_and_spices = request.form.getlist("herbs_or_spices")
-        recipe_collection["herbs_or_spices"] = herbs_and_spices
+        meal_collection["herbs_or_spices"] = herbs_and_spices
         mongo.db["images"].insert_one(image_collection)
-        mongo.db["recipes"].insert_one(recipe_collection)
-        return redirect("/recipes")
+        mongo.db["meals"].insert_one(meal_collection)
+        return redirect("/meals")
     else:
-        meat_images = mongo.db["images"].find({"animal_name": { "$exists": True }})
+        meat_images = mongo.db["images"].find({"animal_name":
+                                              {"$exists": True}})
         vegetables = mongo.db["vegetables"].find()
         herbs = mongo.db["herbs"].find()
         spices = mongo.db["spices"].find()
         vegetable = []
         for veg in vegetables:
             vegetable.append(veg["vegetable_name"])
-  
+
         meat_options = {}
         for image in meat_images:
             name = image["animal_name"]
@@ -147,88 +190,59 @@ def add_recipe():
             animals = mongo.db["animals"].find({"animal_name": name})
             for animal in animals:
                 cut_names.append(animal["cut_name"])
-            
+
             meat_options[name] = cut_names
-            
-        return render_template("add_recipe.html", meat_options=meat_options, vegetables=vegetable, herbs=herbs, spices=spices)
-        
-@app.route("/recipe/<recipe_name>/<recipe_id>/") 
-def show_recipe(recipe_name, recipe_id):
-    recipe = mongo.db["recipes"].find({"recipe_name" : recipe_name})
-    images = mongo.db["images"].find({"recipe_name": recipe_name})
-    
+
+        return render_template("add_meal.html",
+                               meat_options=meat_options,
+                               vegetables=vegetable,
+                               herbs=herbs, spices=spices)
+
+# --------------- Ingredients and nutritional info for meals ---------------
+
+
+@app.route("/meal/<meal_name>/<meal_id>/")
+def show_meal(meal_name, meal_id):
+    meal = mongo.db["meals"].find({"meal_name": meal_name})
+    images = mongo.db["images"].find({"meal_name": meal_name})
+
     cals = []
     carbs = []
     fat = []
     protein = []
-    for r in recipe:
+    for r in meal:
         vegetables = r["vegetables"]
-        
+
         for veg, info in vegetables.items():
             cals.append(float(info["calories"]))
             carbs.append(float(info["carbs"]))
             fat.append(float(info["fat"]))
             protein.append(float(info["protein"]))
-            
+
         meats = r["meats"]
         for meat, info in meats.items():
             cals.append(float(info["calories"]))
             carbs.append(float(info["carbs"]))
             fat.append(float(info["fat"]))
             protein.append(float(info["protein"]))
-        recipe_name = r["recipe_name"]
-        recipe_id = r["_id"]
-        recipe_meats = r["meats"]
-        recipe_vegetables = r["vegetables"]
-        recipe_seasoning = r["herbs_or_spices"]
-        
+        meal_name = r["meal_name"]
+        meal_id = r["_id"]
+        meal_meats = r["meats"]
+        meal_vegetables = r["vegetables"]
+        meal_seasoning = r["herbs_or_spices"]
+
     total_cals = round(sum(cals), 1)
     total_carbs = round(sum(carbs), 1)
     total_fat = round(sum(fat), 1)
     total_protein = round(sum(protein), 1)
-    return render_template("recipe.html", images=images, recipe_name=recipe_name, recipe_id=recipe_id, recipe_meats=recipe_meats, recipe_vegetables=recipe_vegetables, recipe_seasoning=recipe_seasoning, total_cals=total_cals, total_carbs=total_carbs, total_fat=total_fat, total_protein=total_protein)
-    
-@app.route("/<recipe_name>/<recipe_id>/edit/recipe", methods=["POST", "GET"])        
-def edit_recipe(recipe_name, recipe_id):
-    if request.method == "POST":
-        form_values = request.form.to_dict()
-        mongo.db["recipes"].update({"_id": ObjectId(recipe_id)}, form_values)
-        
-        if form_values["recipe_name"] != recipe_name:
-            the_recipe = mongo.db["recipes"].find_one({"_id": ObjectId(recipe_id)})
-            mongo.db["recipes"].remove(the_recipe)
-            mongo.db[form_values["recipe_name"]].insert(recipe_name)
-            
-        return redirect(url_for("show_recipes", recipe_name=form_values["recipe_name"]))
-    else:
-        the_recipe =  mongo.db["recipes"].find_one({"_id": ObjectId(recipe_id)})
-        meat_images = mongo.db["images"].find({"animal_name": { "$exists": True }})
-        veg_images = mongo.db["images"].find({"vegtable_name": { "$exists": True }})
-        herbs = mongo.db["herbs"].find()
-        spices = mongo.db["spices"].find()
-        
-        meat_options = {}
-        for image in meat_images:
-            name = image["animal_name"]
-            cut_names = []
-            animals = mongo.db["animals"].find({"animal_name": name})
-            for animal in animals:
-                cut_names.append(animal["cut_name"])
-            
-            meat_options[name] = cut_names
-            
-        veg_options = {}
-        for image in veg_images:
-            name = image["vegtable_name"]
-            veg_names = []
-            vegetables = mongo.db["vegetables"].find({"vegtable_name": name})
-            for vegtable in vegetables:
-                veg_names.append(vegtable["veg_option"])
-            
-            veg_options[name] = veg_names
-            
-        return render_template('edit_recipe.html', recipe=the_recipe, meat_options=meat_options, veg_options=veg_options, herbs=herbs, spices=spices)
+    return render_template("meal.html",
+                           images=images, meal_name=meal_name,
+                           meal_id=meal_id, meal_meats=meal_meats,
+                           meal_vegetables=meal_vegetables,
+                           meal_seasoning=meal_seasoning,
+                           total_cals=total_cals, total_carbs=total_carbs,
+                           total_fat=total_fat, total_protein=total_protein)
 
 if __name__ == "__main__":
-        app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), debug=True)
-
+        app.run(host=os.getenv('IP', '0.0.0.0'),
+                port=int(os.getenv('PORT', 8080)), debug=True)
